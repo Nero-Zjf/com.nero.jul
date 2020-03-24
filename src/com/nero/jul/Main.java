9b96ea818584e8eaf7d031a4005bcb8b866f02d9
ZengJunFeng <zengjunfeng9981@163.com>
2020-03-24 09:43:19 +0800

    初始提交

---
+++ src/com/nero/jul/Main.java
@@ -0,0 +1,12 @@
+package com.nero.jul;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Logger.getGlobal().info("--------------info---------------------");
+        Logger.getGlobal().warning("--------------warningl---------------------");
+    }
+}
